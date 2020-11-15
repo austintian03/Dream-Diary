73b5726e476882737ced91c99c27daed9d510ad3
Austin Tian <adt339@nyu.edu>
2020-11-14 21:22:11 -0500

    Implement authentication using passport-local

--- app.js
+++ app.js
@@ -1,24 +1,41 @@
 require('./db');
-
 const express = require('express');
 const path = require('path');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 //const bodyParser = require('body-parser');
 
 const app = express();
 
 const mongoose = require('mongoose');
 const Dream = mongoose.model('Dream');
+const User = mongoose.model('User');
+
+//authentication configuration
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 // enable sessions
-/*
-const session = require('express-session');
 const sessionOptions = {
     secret: 'secret cookie thang (store this elsewhere!)',
-    resave: true,
+    resave: false,
     saveUninitialized: false
 };
 app.use(session(sessionOptions));
-*/
+
+//initilialize passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+//make user data available to all templates
+app.use((req, res, next) => {
+    //res.locals.message;
+    res.locals.user = req.user;
+    next();
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -30,8 +47,21 @@ app.use(express.urlencoded({ extended: false }));
 // serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+//route handlers
 app.get('/', (req, res) => {
-    Dream.find((err, dreams) => {
+    if(req.user) {
+        res.redirect('/dreams');
+    }
+    else{
+        res.render('index', {message: req.session.message});
+    }
+});
+
+app.get('/dreams', (req, res) => {
+    if(!req.user) { res.redirect('/') };
+
+    Dream.find({user: req.user._id}, (err, dreams) => {
         if(err) {
             console.log(err);
         }
@@ -42,8 +72,10 @@ app.get('/', (req, res) => {
 });
 
 app.get('/dreams/:dreamSlug', (req, res) => {
+    if(!req.user) { res.redirect('/') };
+
     const slug = req.params.dreamSlug;
-    Dream.findOne({myslug: slug}, (err, dream) => {
+    Dream.findOne({user: req.user._id, myslug: slug}, (err, dream) => {
         if(err) {
             console.log(err);
         }
@@ -52,13 +84,17 @@ app.get('/dreams/:dreamSlug', (req, res) => {
         }
     });
 });
+//todo: app.post('/dreams/:dreamSlug') using Dream.findOneAndUpdate()
 
 app.get('/record', (req, res) => {
+    if(!req.user) { res.redirect('/') };
+
     res.render('record');
 });
 
 app.post('/record', (req, res) => {
     const newDream = new Dream({
+        user: req.user._id,
         date: req.body.date.toString(),
         title: req.body.title,
         dream: req.body.dream,
@@ -67,12 +103,47 @@ app.post('/record', (req, res) => {
 
     newDream.save((err) => {
             if(!err) {
-                res.redirect('/');
+                res.redirect('/dreams');
             }
             else {
                 console.log(err);
             }
     });
+});
+
+app.post('/login', function(req,res,next) {
+    passport.authenticate('local', function(err,user) {
+        if(user) {
+            req.logIn(user, function(err) {
+                res.redirect('/dreams');
+            });
+        } 
+        else {
+            req.session.message = 'Your login or password is incorrect.';
+            res.redirect('/');
+            //res.render('index', {message:'Your login or password is incorrect.'});
+        }
+    })(req, res, next);
+});
+  
+app.post('/register', function(req, res) {
+    User.register(new User({username:req.body.username}), req.body.password, function(err, user){
+        if (err) {
+            req.session.message = 'Your registration information is not valid';
+            res.redirect('/');
+            //res.render('index', {message:'Your registration information is not valid'});
+        } 
+        else {
+            passport.authenticate('local')(req, res, function() {
+                res.redirect('/dreams');
+            });
+        }
+    });   
+});
+
+app.post('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/');
 });
 
 app.listen(process.env.PORT || 3000);
--- db.js
+++ db.js
@@ -1,23 +1,25 @@
 // 1ST DRAFT DATA MODEL
 const mongoose = require('mongoose');
 const URLSlugs = require('mongoose-url-slugs')
+const passportLocalMongoose = require('passport-local-mongoose');
 //mongoose.promise = global.Promise;
 // users
 // * our site requires authentication...
 // * so users have a username and password
 // * can have 0 or more dreams
-/*
-const User = new mongoose.Schema({
+
+const UserSchema = new mongoose.Schema({
   //username provided by authentication plugin
   //password hash provided by authentication plugin
-  dreams: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Dream'}]
-});*/
+  //dreams: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Dream'}]
+});
+UserSchema.plugin(passportLocalMongoose);
 
 // a dream
 // * each dream must have a related user, date/timestamp, and dream text
 // * thoughts are optional and can be added later
 const DreamSchema = new mongoose.Schema({
-  //user: {type: mongoose.Schema.Types.ObjectId, ref:'User'},
+  user: {type: mongoose.Schema.Types.ObjectId, ref:'User'},
   date: {type: String, default: Date.now()},
   title: String,
   dream: String,
@@ -25,18 +27,21 @@ const DreamSchema = new mongoose.Schema({
 });
 
 DreamSchema.plugin(URLSlugs('date title', {field: 'myslug'}));
-//mongoose.model('User', User);
+mongoose.model('User', UserSchema);
 mongoose.model('Dream', DreamSchema);
 
-const uri = process.env.MONGODB_URI || "mongodb+srv://adt339:MYjtx24yas7C75qb@dreamdb.a55jq.mongodb.net/dreamdiarydb?retryWrites=true&w=majority";
+const uri = process.env.MONGODB_URI || 'mongodb://localhost/dreamTester' || 'mongodb+srv://adt339:MYjtx24yas7C75qb@dreamdb.a55jq.mongodb.net/dreamdiarydb?retryWrites=true&w=majority';
 mongoose.Promise = global.Promise;
 
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true});
 
+
+
+/*
 mongoose.connection.on('connected', () => {
     console.log('Mongoose is connected...');
 });
-
+*/
 
 /*
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true})
